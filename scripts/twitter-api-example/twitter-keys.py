83a5c7894cc70fec40ed37ec2af8963be915a89d
yeshivadataanalytics <64369309+yeshivadataanalytics@users.noreply.github.com>
2020-04-26 23:00:30 -0400

    Create twitter-keys.py

---
+++ scripts/twitter-api-example/twitter-keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+#these are client keys to call a twitter api, use import twitter_keys after saving this to your Lib directory
+client_key = 'WT2ZgK9mYqOYWFuvYxwrwSBBA'
+client_secret = 'zcwTn9PjAx49Y32FULq8J4RaVxki6ROrM2vOuwukVRtU7lzwZH'
+
